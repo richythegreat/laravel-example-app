ea0dd68d9c32a5cd7a33872c7a3c4c6eb2810d32
ricards <ricards@gmail.com>
2024-09-30 14:42:51 +0300

    author title and content fields added

--- example-app/database/migrations/2024_09_30_113248_create_posts_table.php
+++ example-app/database/migrations/2024_09_30_113248_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('Author');
+            $table->string ('Title');
+            $table->longtext('Content');
             $table->timestamps();
         });
     }
